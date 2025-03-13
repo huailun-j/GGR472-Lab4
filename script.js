95c48ee4d98e3b02bb3b05f17ce5b963ebcc64b3
huailun-j <huailun.jiang@mail.utoronto.ca>
2025-03-12 22:00:38 -0400

    add

--- script.js
+++ script.js
@@ -68,13 +68,73 @@ fetch('https:///huailun-j.github.io/GGR472-Lab4/data/pedcyc_collision_06-21.geoj
 map.on('load', () => {
     console.log("Collision data loaded:", collisiongeojson);
 
+    let bboxgeojson;
+    let bbox = turf.envelope(collisiongeojson); 
+    let bboxscaled = turf.transformScale(bbox, 1.10); //increasing the size of the bounding box by 10%
+    console.log("Expanded Bounding Box:", bboxscaled);
 
+    // create empty geojson objects to hold feature
+    bboxgeojson = {
+        "type": "FeatureCollection",
+        "feature": [bboxscaled]
 
+    }
+    
+    // get the min and max coordinates of both axis the bounding box. Store as array of coordinates [minX, minY, maxX, maxY]
+    let bboxcoords = [
+        bboxscaled.geometry.coordinates[0][0][0], // minX
+        bboxscaled.geometry.coordinates[0][0][1], // minY
+        bboxscaled.geometry.coordinates[0][2][0], // maxX
+        bboxscaled.geometry.coordinates[0][2][1]  // maxY
+    ];
+    console.log("Bounding Box Coordinates:", bboxcoords);
+
+    // Create a grid of 0.5km hexagons inside the spatial limits of the bbox coordinates
+    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: "kilometers"});
+
+    
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
 --------------------------------------------------------------------*/
 //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
 //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
+
+    // aggregate the point data
+    let collishex = turf.collect(hexgeojson, collisiongeojson, '_id', 'values');
+    //create a foreach loop
+    let maxcollis = 0; // create new variable to storemax no. collisions
+    collishex.features.forEach((feature) => { 
+        feature.properties.COUNT = feature.properties.values.length //count the number of collisions
+        if (feature.properties.COUNT > maxcollis) { //if the COUNT value is bigger than the maxcollis collision value
+            console.log(feature); 
+            maxcollis = feature.properties.COUNT // to updates the maxcollision value 
+        }
+    });
+
+
+    //5
+
+    map.addSource('collis-hex', {
+        type: 'geojson',
+        data: hexgeojson
+    });
+
+    map.addLayer({
+        'id': 'collis-hex-fill',
+        'type': 'fill',
+        'source': 'collis-hex',
+        'paint': {
+            'fill-color': [
+                'step', //step expression
+                ['get', 'COUNT'],
+                '#F9E79F',
+                10, '#F39C12',
+                25, '#D35400'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': "white"
+        }
+    });
 
 
 
